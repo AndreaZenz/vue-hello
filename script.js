3a9f8a3c9ba3cab14dd433db018e853f071e34b5
AndreaZenz <zenzero.ere@gmail.com>
2021-04-29 14:18:52 +0200

    adding text using data

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-data
+// Descrizione:
+// Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+// Bonus:
+// Aggiungere alla pagina un’immagine, presa anch’essa da un data.
+
+const app = new Vue(
+    {
+        el: '#vueContainer',
+        data: {
+            messaggio: "Ciao dall'interno del data",
+        }
+    }
+);
